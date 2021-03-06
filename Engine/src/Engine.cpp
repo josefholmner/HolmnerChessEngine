0778c08f1c774de3ef1f9f79759c3d61e691a9f2
josefholmner <josef.holmner@gmail.com>
2021-03-06 20:30:21 +0100

    Add basic directory structure with skeleton cmake files and source

---
+++ Engine/src/Engine.cpp
@@ -0,0 +1,8 @@
+#include "Engine.h"
+
+#include <iostream>
+
+void Engine::printDummy()
+{
+    std::cout << "Dummy print from Engine." << std::endl;
+}
---
+++ HolmnerChessGui/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "Engine.h"
+
+int main()
+{
+    std::cout << "Dummy printout from main." << std::endl;
+    Engine::printDummy();
+    return 0;
+}
